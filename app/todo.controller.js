1d0cd289d42800eed4c84f275b744723d58cc3df
b1collector <b1collector@gmail.com>
2016-12-18 17:09:44 -0500

    Allow to do items to be marked as completed

--- app/todo.controller.js
+++ app/todo.controller.js
@@ -7,11 +7,22 @@
 
    ctlr.$inject = ["$scope", "todoRepo"];
 
+   var doneStyle = {"text-decoration":"line-through"};
+
 	function ctlr ($scope, todoRepo){
       $scope.todos = [];
 
       todoRepo.getAll().then(function(data){
       	$scope.todos = data;
       });
+
+      $scope.markAsDone = function(index){
+         $scope.todos[index].done = true;
+      };
+
+      $scope.todoStyle = function (done){
+         return done ? doneStyle : {};
+      };
+
 	}
 }())
